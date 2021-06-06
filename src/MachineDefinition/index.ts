76f430929c64f55633785a3b190e00e0a9661b27
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2021-06-06 07:48:25 +0530

    refactor: inline `MachineExtras.*`

--- src/MachineDefinition/index.ts
+++ src/MachineDefinition/index.ts
@@ -1,5 +1,4 @@
 import { O, A, U, L, B, Type, S, F } from "../extras";
-import MachineExtras from "../MachineExtras";
 import { ReferencePathString } from "../universal";
 
 export default MachineDefinition;
@@ -413,12 +412,66 @@ namespace MachineDefinition {
       NodeReferencePathString = ReferencePathString.FromDefinitionPath<L.Popped<Path>>
     > =
       ( context: "TODO"
-      , event: MachineExtras.EntryEventForStateNode<
+      , event: EntryEventForStateNode<
           Precomputed.Get<Precomputed, "DesugaredDefinition">,
           Precomputed,
           NodeReferencePathString
         >
       ) => void
+
+    type EntryEventForStateNode<D, P, StateNodeReferencePathString> =
+      EntryEventForStateNodeWithRoot<D, P, StateNodeReferencePathString, "">
+  
+    type EntryEventForStateNodeWithRoot<D, P, StateNodeReferencePathString, RootReferencePathString,
+      Root = ReferencePathString.ToNode<RootReferencePathString, D>,
+      On = O.Get<Root, "on">,
+      Always = O.Get<Root, "always">,
+      States = O.Get<Root, "states">,
+      EventSchema = O.Get<D, ["schema", "events"]>
+    > =
+      | { [E in keyof On]: 
+          O.Get<On, [E, number]> extends infer T
+            ? [T] extends [never] ? never :
+              T extends any
+                ? ( L.IncludesSubtype<
+                      ReferencePathString.Tuple.Unresolved.ResolveWithStateNode<
+                        D, P, O.Get<T, "target">, RootReferencePathString
+                      >,
+                      | StateNodeReferencePathString 
+                      | ( O.Get<T, "internal"> extends false
+                            ? ReferencePathString.Child<StateNodeReferencePathString, D>
+                            : never
+                        )
+                    > extends true 
+                      ? EventSchema extends undefined ? { type: E } :
+                        U.Extract<EventSchema, { type: E }>
+                      : never
+                  )
+                : never
+            : never
+        }[keyof On]
+      | ( O.Get<Always, number> extends infer T
+            ? [T] extends [never] ? never :
+              L.IncludesSubtype<
+                ReferencePathString.Tuple.Unresolved.ResolveWithStateNode<
+                  D, P, O.Get<T, "target">, RootReferencePathString
+                >,
+                | StateNodeReferencePathString
+                | ( O.Get<T, "internal"> extends false
+                      ? ReferencePathString.Child<StateNodeReferencePathString, D>
+                      : never
+                  )
+              > extends true
+                ? EntryEventForStateNode<D, P, RootReferencePathString>
+                : never
+            : never
+        )
+      | { [C in keyof States]:
+            EntryEventForStateNodeWithRoot<
+              D, P, StateNodeReferencePathString,
+              ReferencePathString.Append<RootReferencePathString, C>
+            >
+        }[keyof States]
   }
 
 
