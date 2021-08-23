c42ea4b8867c7fa495c104677bd72302f525b911
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2021-08-24 01:57:45 +0530

    publish script et al

---
+++ publish.js
@@ -0,0 +1,20 @@
+// @ts-check
+const sh = require("shelljs")
+const rl = require("readline-sync");
+
+if (!rl.keyInYNStrict("you sure?")) process.exit();
+
+sh.ls().includes("dist") && sh.rm("-r", "dist");
+sh.exec("yarn tsc")
+sh.cp("LICENSE", "dist/LICENSE")
+sh.cp("README.md", "dist/README.md");
+sh.cp("package.json", "dist/package.json");
+sh.cd("dist");
+sh.exec("npm publish")
+sh.exec(`git tag ${
+  sh.cat("package.json")
+  .grep("version")
+  .sed(/.*"version": "(.*)".*/, "v$1")
+  .toString()
+}`)
+sh.rm("-r", "dist")
