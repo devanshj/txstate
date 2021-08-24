f234f9c5867ff52762821e61189c6405867d0912
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2021-08-24 07:57:02 +0530

    *.test too

--- publish.js
+++ publish.js
@@ -10,7 +10,12 @@ sh.cp("LICENSE", "dist/LICENSE")
 sh.cp("README.md", "dist/README.md");
 sh.cp("package.json", "dist/package.json");
 sh.cd("dist");
-sh.echo("*.spec.js\n*.spec.d.ts").to(".npmignore")
+sh.echo([
+  "*.spec.js",
+  "*.spec.d.ts",
+  "*.test.js",
+  "*.test.d.ts"
+].join("\n")).to(".npmignore")
 sh.exec("npm publish")
 sh.cd("..")
 sh.rm("-r", "dist")
