4f9e0683c5f3d074140516dfac7d5699d46841be
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2021-08-24 07:53:04 +0530

    use npmignore to exclude specs

--- publish.js
+++ publish.js
@@ -10,6 +10,7 @@ sh.cp("LICENSE", "dist/LICENSE")
 sh.cp("README.md", "dist/README.md");
 sh.cp("package.json", "dist/package.json");
 sh.cd("dist");
+sh.echo("*.spec.js\n*.spec.d.ts").to(".npmignore")
 sh.exec("npm publish")
 sh.cd("..")
 sh.rm("-r", "dist")
